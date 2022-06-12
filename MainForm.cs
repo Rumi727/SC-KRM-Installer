eb3cd36f204b201c754083f9baa7b2e89576481e
Simsimhan Chobo <jun32820@gmail.com>
2022-06-12 23:35:00 +0900

    안쓰는 네임스페이스 삭제

--- MainForm.cs
+++ MainForm.cs
@@ -6,14 +6,8 @@ using SCKRM.Language;
 using System;
 using System.Collections.Generic;
 using System.ComponentModel;
-using System.ComponentModel.Design;
-using System.Data;
-using System.Drawing;
 using System.IO;
-using System.Linq;
 using System.Net;
-using System.Text;
-using System.Threading;
 using System.Threading.Tasks;
 using System.Windows.Forms;
 
